d04068295dbcb19b78a405f0e1aa5396b4fbc0f5
Jonathan-X-Bananaman <xuj4454@my.sduhsd.net>
2026-04-27 20:53:33 +0000

    made clubs popup

--- src/ClubList.jsx
+++ src/ClubList.jsx
@@ -1,25 +1,38 @@
 import './ClubList.css'
-import {usestate} from 'react'
+import {useState} from 'react'
 function ClubList({name,description,categories,time,location}) {
+  const [isOpen, setIsOpen] = useState(false);
   return (
-    <div className="Card">
-      <div className="top">
-        <h1>{name}</h1>
-        <h3>{time}, @{location}</h3>  
-      </div>
-      
-      <i className="heart"></i>
-      {/*{categories.map((item,index)=>(
-        <div key={item} className="card">
-          <h3>{item.title}</h3>
-          <p>{item.content}</p>
+    <>
+      <div className="Card" 
+          onClick={() => setIsOpen(true)} 
+          style={{ cursor: 'pointer' }}>
+        <div className="top">
+          <h1>{name}</h1>
+          <h3>{time}, @{location}</h3>  
         </div>
-      ))}*/}
-      <h5>#{categories}</h5>
-      <div id="clubdesc">
-        <p>{description}</p>
+        
+        <i className="heart"></i>
+        {/*{categories.map((item,index)=>(
+          <div key={item} className="card">
+            <h3>{item.title}</h3>
+            <p>{item.content}</p>
+          </div>
+        ))}*/}
+        <h5>#{categories}</h5>
+        <div id="clubdesc">
+          <p>{description}</p>
+        </div>
       </div>
-    </div>
+      {isOpen && (
+        <div className="popup-overlay" onClick={() => setIsOpen(false)}>
+          <div className="popup-content" onClick={(e) => e.stopPropagation()}>
+            <h2>Popup Content</h2>
+            <button onClick={() => setIsOpen(false)}>Close</button>
+          </div>
+        </div>
+      )}
+    </>
   )
 }
 /*      <div className='Card'>
