3a2874e575c7706ea8dba053b7ca87ec9cff75e4
Jonathan-X-Bananaman <xuj4454@my.sduhsd.net>
2026-05-01 20:53:14 +0000

    updated the events page, fixed spacing issues

---
+++ src/ClubEvents.jsx
@@ -0,0 +1,17 @@
+import ClubList from './ClubList.jsx'
+import './Discover.css'
+function ClubEvents(){
+    return(
+        <div>
+            <div id="clubs">
+                <ClubList name="A Club" description="We draw As" categories="fun" time="never" location="CCA"/>
+                <ClubList name="B Club" description="We draw Bs" categories="fun" time="never" location="CCA"/>
+                <ClubList name="C Club" description="We draw Cs" categories="fun" time="never" location="CCA"/>
+                <ClubList name="D Club" description="We draw Ds" categories="fun" time="never" location="CCA"/>
+                <ClubList name="E Club" description="We draw Es" categories="fun" time="never" location="CCA"/>
+            </div>
+        </div>
+    )
+}
+
+export default ClubEvents;
